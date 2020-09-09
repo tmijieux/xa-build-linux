44df98c48ea529805a94bc69994397abdbe81718
Thomas Mijieux <thomas.mijieux@hotmail.fr>
2020-09-09 21:51:26 +0200

    replace my fork of xamarin-android with upstream since it now supports archlinux directly

--- build-xa.sh
+++ build-xa.sh
@@ -2,7 +2,7 @@
 set -x
 set -e
 cd /home/builduser
-git clone https://github.com/tmijieux/xamarin-android.git || true
+git clone https://github.com/xamarin/xamarin-android.git || true
 cd xamarin-android
 make prepare
 make
